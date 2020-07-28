1dfad2a3e78e5989ab4e42028c1ec0043b913c7f
Tamy Vial <vialdesouza@gmail.com>
2020-07-27 20:48:03 -0400

    first commit

---
+++ src/components/menu/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../assets/img/logo.png'
+import './Menu.css'
+import Button from '../Button'
+
+
+function Menu() {
+    return <nav className='Menu'>
+        <a href='/'>
+            <img src={logo} className='Logo' />
+        </a>
+        <Button as='a' href="/" className='ButtonLink'>
+            Novo vídeo
+        </Button>
+    </nav>
+}
+
+export default Menu;
